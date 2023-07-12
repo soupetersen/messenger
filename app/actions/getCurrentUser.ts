4269bdde61687a3e31e3eda8b9b7ec6f19e101bf
soupetersen <soupetersen@gmail.com>
2023-07-12 12:04:17 -0300

    Add some actions

---
+++ app/actions/getCurrentUser.ts
@@ -0,0 +1,21 @@
+import prisma from "@/app/libs/prismadb";
+
+import getSession from "./getSession";
+
+const getCurrentUser = async () => {
+  try {
+    const session = await getSession();
+    if (!session?.user?.email) return null;
+
+    const user = await prisma.user.findUnique({
+      where: { email: session.user.email },
+    });
+
+    return user;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
+
+export default getCurrentUser;
